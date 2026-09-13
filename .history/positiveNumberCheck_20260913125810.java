a4b7b886ab9bb7d607fe717cbfa0cc4e81cf61e8
Shraddha7001 <yshraddha729@gmail.com>
2026-09-13 13:03:50 +0530

    Add Positive Number Check Program...
    Write a Java program that takes an integer input from the user and checks whether the number
    is positive.

---
+++ .history/positiveNumberCheck_20260913125810.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class positiveNumberCheck{
+
+    public static void main(String[] args) {
+        
+        System.out.print("Enter a number : ");
+        Scanner sc = new Scanner(System.in);
+
+        long num = sc.nextInt();
+
+        if(num > 0){
+
+            System.out.println("It is a positive number");
+        }
+        else{
+            System.out.println("It is not a positive number");
+        }
+
+        sc.close();
+    }
+}
