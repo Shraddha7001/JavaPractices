f67101336bc198070fca29491be994ab37c6630c
Shraddha7001 <yshraddha729@gmail.com>
2026-09-13 14:00:03 +0530

    Add a program to check eligibility to vote.
    Write a Java program that takes age as input and checks whether the person is eligible to vote.

---
+++ voting.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class voting {
+
+    public static void main(String[] args) {
+        
+        System.out.print("Enter the age of a person = ");
+
+        Scanner sc = new Scanner(System.in);
+
+        int age = sc.nextInt();
+
+        if(age >= 18){
+
+            System.out.println("Person is eligible to vote");
+        }
+        else{
+            System.out.println("Person is not eligible to vote");
+        }
+    }
+    
+}
