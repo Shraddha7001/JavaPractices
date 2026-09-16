db5a17a6c612ef3c5144ed9122748719be8af3ec
Shraddha7001 <yshraddha729@gmail.com>
2026-09-16 10:38:16 +0530

    Add a calculator program.
    Write a Java program that takes:
    ● two numbers
    ● one operator (+, -, *, /,%)
    and performs the operation using switch.

---
+++ calculator.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class calculator {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter 1st number = ");
+        double num1 = sc.nextDouble();
+
+
+        System.out.print("Enter 2nd number = ");
+        double num2 = sc.nextDouble();
+
+        System.out.print("Enter operator (+,-,*,/,%) = ");
+        char operator = sc.next().charAt(0);
+
+        switch (operator) {
+            case '+': System.out.println("Add " + num1 + " and " + num2 + " = " + (num1 + num2));    
+                break;
+            case '-': System.out.println("Subtract " + num1 + " and " + num2 + " = " + (num1 - num2));    
+                break;
+            case '*': System.out.println("Multiply " + num1 + " and " + num2 + " = " + (num1 * num2));    
+                break;
+            case '/': System.out.println("Divide " + num1 + " and " + num2 + " = " + (num1 / num2));    
+                break;
+            case '%': System.out.println("Modulus " + num1 + " and " + num2 + " = " + (num1 % num2));    
+                break;
+
+        
+            default: System.out.println("Invalid operator...!");
+                break;
+        }
+
+        sc.close();
+    }
+    
+}
